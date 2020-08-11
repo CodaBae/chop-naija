d13696ff61b86b3026e7ddfeedf808e6504933f8
codabae <shalom.mat40@gmail.com>
2020-08-11 15:16:56 +0100

    first

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const Product = require('../models/Product');
+const User = require('../models/User');
+
+ 
+// Welcome Page
+router.get('/', (req, res) => {
+  Product.find({}, function (err, product) {
+    let url = req.protocol + '://' + req.get('host') + req.originalUrl
+    let id = url.replace("http://chopnaija.com/?id=", '')
+
+    console.log()
+
+    if (id === 'http://chopnaija.com/'){
+    
+    req.flash(
+      'success_msg',
+      'you need to login or register'
+    );
+    res.redirect('/users/login');
+    }else{
+
+      User.findOne({ _id: id }).then(user => {
+        res.render('index', { product: product, user:user.cart });
+  
+      }) 
+    }
+  });
+});
+
+// Dashboard
+router.get('/vendor', ensureAuthenticated, (req, res) => {
+  res.render('vendor', {
+    user: req.user
+  })
+
+
+});
+
+
+router.get('/add_product', ensureAuthenticated, (req, res) =>
+  res.render('addProducts', {
+    user: req.user
+  })
+);
+
+
+
+
+module.exports = router;
